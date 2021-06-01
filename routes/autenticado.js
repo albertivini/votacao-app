f6b8a6769726189eb421a23729640c09d37cb5de
Vinicius Alberti <73914923+albertivini@users.noreply.github.com>
2021-06-01 18:55:31 -0300

    Refatorando código

--- routes/autenticado.js
+++ routes/autenticado.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 
 // rota para saber se a autenticacao ta funcionando
 router.get('/', function(req, res, next) {
--- routes/forgot.js
+++ routes/forgot.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db')
-const validacao = require('../validacao')
 
 // rota get para recuperação de senha
 router.get('/', function(req, res) {
@@ -10,7 +9,7 @@ router.get('/', function(req, res) {
   
 // gera uma nova senha e envia para o email da pessoa
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
 
       db.findUser(req.body.email, (err, doc) => {
         // busca no banco o email passado pelo cliente
@@ -18,11 +17,11 @@ router.post('/', function(req, res, next) {
           res.redirect('/forgot') 
           // erro para demonstrar que usuario nao foi encontrado
         } else {
-          const newpass = require('../utils').generatePassword()
+          const newpass = require('../utils/functions').generatePassword()
           // gera nova senha e guarda em newpass
           db.changePassword(req.body.email, newpass)
           // atualiza o banco com a nova senha
-          require('../mail')(req.body.email, 'Nova senha', 'Olá, sua nova senha é: ' + newpass )
+          require('../utils/mail')(req.body.email, 'Nova senha', 'Olá, sua nova senha é: ' + newpass )
           res.redirect('/forgot')
           // envia e-mail com a senha nova
         }
