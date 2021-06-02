dd08bc76ba38412645cfc36d6751d592b8390492
Bruno-Castilhos <77765988+Bruno-Castilhos@users.noreply.github.com>
2021-06-01 22:38:59 -0300

    modificando view dos votos

--- routes/votacao.js
+++ routes/votacao.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 
 router.get('/', function(req, res) {
-    res.render('votacao', {title: "BIG BROTHER BRASIL"})
+    res.render('votacao', {title: "Paredão BBBXX: Vote para eliminar. Participante1, Participante2 ou Participante3?"})
 })
 
 module.exports = router
