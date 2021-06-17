1a63545246c7912223bee95567996e2ff1f6f94f
Vinicius Alberti <73914923+albertivini@users.noreply.github.com>
2021-06-17 20:05:26 -0300

    Refatorando o projeto

--- routes/autenticado.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// rota para saber se a autenticacao ta funcionando
-router.get('/', function(req, res, next) {
-  res.render('autenticado', { title: 'Autenticado', profile: req.user.profile });
-});
-
-module.exports = router;
--- routes/votacao.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-router.get('/', function(req, res) {
-    res.render('votacao', {title: "Paredão BBBXX: Vote para eliminar. Participante1, Participante2 ou Participante3?"})
-})
-
-module.exports = router
